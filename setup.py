87a6eb32e39928e0234cd13044735f915c86dd17
Abdul Muiz <muizabdul29@gmail.com>
2026-04-20 12:05:56 +0200

    Version bumped

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name='pybatteryse',
-    version='1.0.0',
+    version='1.0.1',
     author='Muiz Sheikh',
     description='Battery State Estimation in Python',
     long_description=readme,
